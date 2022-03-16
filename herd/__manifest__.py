6b0704d83923d26b63f445c9e62a51f6680aa868
David Beal <david.beal@akretion.com>
2022-03-16 09:36:03 +0100

    ADD herd module

---
+++ herd/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2022 Akretion
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Herd",
+    "version": "14.0.1.0.0",
+    "category": "Tools",
+    "website": "https://github.com/OCA/vertical-agriculture",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "maintainers": ["bealdav"],
+    "license": "AGPL-3",
+    "installable": True,
+    "summary": "Animal Herd management",
+    "depends": [
+        "animal_owner",
+    ],
+    "data": [
+        "views/animal.xml",
+        "views/herd.xml",
+        "views/specie.xml",
+        "data/animal_species.xml",
+        "security/ir.model.access.csv",
+    ],
+    "demo": [],
+}
---
+++ herd/readme/DESCRIPTION.rst
@@ -0,0 +1,8 @@
+Manage animal herds and relative informations:
+
+- animal definition (extends animal module)
+- specie classification (possible to define arborescent structure)
+
+.. figure:: ../static/description/marguerite.png
+
+.. figure:: ../static/description/bois-joli.png
