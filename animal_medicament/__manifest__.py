cbaaf779c775b2f604b56d1d8aefb7f87a79cff4
David Beal <david.beal@akretion.com>
2023-04-06 17:48:42 +0200

    [14.0] ADD animal_medicament

---
+++ animal_medicament/__manifest__.py
@@ -0,0 +1,23 @@
+# © 2023 David BEAL @ Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Animal Medicament",
+    "version": "14.0.1.1.0",
+    "category": "Tools",
+    "website": "https://github.com/OCA/vertical-agriculture",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "summary": "Drug posologies definition for animals",
+    "depends": [
+        "product",
+        "animal",
+    ],
+    "data": [
+        "views/posology.xml",
+        "views/product.xml",
+        "security/ir.model.access.csv",
+    ],
+    "demo": [],
+}
