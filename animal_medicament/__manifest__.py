38036111d8fae9eb905d8e07071fd80582775cce
MarcusPDL <marcuspaulodiblopes@gmail.com>
2026-07-22 10:36:46 -0300

    [MIG] animal_medicament: Migration to 16.0

--- animal_medicament/__manifest__.py
+++ animal_medicament/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Animal Medicament",
-    "version": "14.0.1.1.0",
+    "version": "16.0.1.0.0",
     "category": "Tools",
     "website": "https://github.com/OCA/vertical-agriculture",
     "author": "Akretion, Odoo Community Association (OCA)",
