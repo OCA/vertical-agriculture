828c498dccbe575eecef3f96eb991218638bd364
Mourad <mourad.elhadj.mimoune@akretion.com>
2026-09-01 11:52:07 +0200

    [MIG] herd: Migration to 18.0

--- herd/__manifest__.py
+++ herd/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Herd",
-    "version": "14.0.1.1.0",
+    "version": "18.0.1.0.0",
     "category": "Animal",
     "website": "https://github.com/OCA/vertical-agriculture",
     "author": "Akretion, Odoo Community Association (OCA)",
